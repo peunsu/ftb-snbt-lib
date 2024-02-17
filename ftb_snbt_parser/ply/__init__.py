24b4cd38d4296e534c611a092da7cd7131f79695
peunsu <44690893+peunsu@users.noreply.github.com>
2024-02-17 11:27:17 +0000

    Implement the parser for the FTB snbt format and add example code.

---
+++ ftb_snbt_parser/ply/__init__.py
@@ -0,0 +1,6 @@
+# PLY package
+# Author: David Beazley (dave@dabeaz.com)
+# https://github.com/dabeaz/ply
+
+__version__ = '3.11'
+__all__ = ['lex','yacc']
---
+++ ftb_snbt_parser/token.py
@@ -0,0 +1,93 @@
+from .ply import lex
+from .tag import *
+    
+tokens = (
+    "LBRACE",
+    "RBRACE",
+    "LBRACKET",
+    "RBRACKET",
+    "BOOL",
+    "BYTE",
+    "DOUBLE",
+    "LONG",
+    "INTEGER",
+    "STRING",
+    "NAME",
+    "COLON",
+    "COMMA",
+)
+
+t_ignore = ' \t'
+
+def t_newline(t):
+    r'\n'
+    t.lexer.lineno += 1
+
+def t_LBRACE(t):
+    r'\{'
+    t.lexer.level += 1
+    return t
+
+def t_RBRACE(t):
+    r'\}'
+    t.lexer.level -= 1
+    return t
+
+def t_LBRACKET(t):
+    r'\['
+    t.lexer.level += 1
+    return t
+
+def t_RBRACKET(t):
+    r'\]'
+    t.lexer.level -= 1
+    return t
+
+def t_BOOL(t):
+    r'true|false'
+    t.value = t.value == "true"
+    return t
+
+def t_BYTE(t):
+    r'\-?[0-9]+b'
+    t.value = Byte(t.value[:-1])
+    return t
+
+def t_DOUBLE(t):
+    r'\-?[0-9]+\.[0-9]+d'
+    t.value = Double(t.value[:-1])
+    return t
+
+def t_LONG(t):
+    r'\-?[0-9]+L'
+    t.value = Long(t.value[:-1])
+    return t
+
+def t_INTEGER(t):
+    r'\-?[0-9]+'
+    t.value = Integer(t.value)
+    return t
+
+def t_STRING(t):
+    r'\"[^\"\\]*(?:\\.[^\"\\]*)*\"'
+    t.value = String(t.value[1:-1])
+    return t
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    return t
+
+def t_COLON(t):
+    r'\:'
+    return t
+
+def t_COMMA(t):
+    r'\,'
+    return t
+
+def t_error(t):
+    print(f"Illegal character {t.value[0]!r}")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+lexer.level = 0
